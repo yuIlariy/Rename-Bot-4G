cd462583f79a5bdee6098df0ac77b10c5213933e
Techify Bots <RahulReviewsYT@gmail.com>
2024-12-25 11:46:33 +0530

    Update script.py

--- script.py
+++ script.py
@@ -20,17 +20,17 @@ class script(object):
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://telegram.me/RahulReviewsYT'>Rahul Reviews</a>
+<b>📢 Channel :</b> <a href='https://telegram.me/TechifyBots'>Techify Bots</a>
 <b>🧑‍💻 Developer :</b> <a href='https://telegram.me/CallOwnerBot'>Rahul</a>
 
-<b>♻️ Bot Made By :</b> @RahulReviewsYT"""
+<b>♻️ Bot Made By :</b> @TechifyBots"""
 
     DONATE_TXT = """
 <b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
 
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
 
-<b>🛍 UPI ID:</b> <code>RahulReviews@UPI</code> 
+<b>🛍 UPI ID:</b> <code>TechifyBots@UPI</code> 
 
 <b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
 
