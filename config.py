b02b22072fabbdecb31b7dafd1f03aa056da1105
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-15 15:00:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+ADMIN = int(os.environ.get("ADMIN", "6318135266"))
 
 
 # Premium 4GB Renaming Client Config
@@ -14,14 +14,14 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "modstorexd")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1925329161"))
 
 
 # Mongo DB Database Config
 DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "TechifyBots")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "ubots")
 
 
 # Other Variables Config
-START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ada3f739fed7efdbe7b08.jpg")
+START_PIC = os.environ.get("START_PIC", "https://i.ibb.co/fzgHjXQn/1752254564132.png")
